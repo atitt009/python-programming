06047c3e53115ec84a8a87f0cde37bb61d9d7246
Atit <atitbhattarai232@gmail.com>
2024-11-05 09:54:23 +0545

    week3 first push

---
+++ main.py
@@ -0,0 +1,6 @@
+import Week_3_Mathmatics #import whole package
+from Week_3_Mathmatics import trigonometry
+import Week_3_Mathmatics.trigonometry
+consine_of_45 = Week_3_Mathmatics.trigonometry.cosine(45)
+trigonometry.cosine(45)
+print(consine_of_45)
