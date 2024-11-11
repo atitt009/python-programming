d1bf2060ad4248e956da472e7a5e67e54893398f
Atit <atitbhattarai232@gmail.com>
2024-11-11 09:29:53 +0545

    Monday

--- math_utils.py
+++ math_utils.py
@@ -1,5 +1,42 @@
-def add(a,b):
-    return a + b
-
-def substraction(a,b):
-    return a - b
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "# Python Module"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# math_utils.py\n",
+    "def add(a, b):\n",
+    "    return a + b\n",
+    "\n",
+    "def\n"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "name": "python",
+   "version": "3.12.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
